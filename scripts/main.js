7bba4afdccedde96a6f5d31548b17e8f3a733c38
Tremayne Pendragin <tpend9@icloud.comn>
2017-04-17 18:05:40 +1000

    this update incudes the cueing of the animation also all animation elements are now in objects

--- scripts/main.js
+++ scripts/main.js
@@ -7,45 +7,39 @@ if (true) {
     
     document.getElementById('cover_img').style.maxHeight = window.innerHeight - 50 + 'px';
     
-    //////////////////////
-    //
-    //     education
-    //
-    //////////////////////
     
-    var pos = window.innerWidth / 1.5;
-    if (window.innerWidth < 768) {
-        for (var i = 0; i < education_cards.length; i++) {
-            education_cards[i].style.left = pos + (pos * i / 4) + 'px';
+    
+    
+    
+    
+    
+    
+    console.log(cue_point);
+    
+    
+    $(document).scroll(function() {
+        var pos = $(window).scrollTop();
+        
+        for (var i = 0; i < cue_point.length; i++) {
+            
+            if (!cue_point[i].active) {
+                if (pos >= cue_point[i].cue_point[0] - 15 && pos <= cue_point[i].cue_point[0] + 15) {
+                    cue_point[i].animation();
+                    cue_point[i].active = true;
+                }
+            }
         }
-    } else {
-        for (var i = 0; i < education_cards.length; i += 2) {
-            education_cards[i].style.left = pos + (pos * i / 9) + 'px';
-            education_cards[i + 1].style.right = pos + (pos * (i + 1) / 9) + 'px';
-        }
-    }
+        
+        
+        
+        
+        
+    });
     
-    
-    
-    //////////////////////
-    //
-    //     awards
-    //
-    //////////////////////
-    
-    $("#awards_img").fadeOut();
-    
-    
-    //////////////////////
-    //
-    //     skills
-    //
-    //////////////////////
-    
-    for (var i = 1; i < skills_levels.length + 1; i++) {
-        for (var j = 1; j <= 3; j++) {
-            document.getElementById('skills_ele_' + i + '_' + j).style.width = '0%';
-        }
-    }
+
+
+
+
+
     
 }
--- scripts/skills.js
+++ scripts/skills.js
@@ -1,21 +1,38 @@
 
-//html css js php myquery
-//50 30 20
-var skills_levels = [
-    [50, 30, 6],
-    [50, 10, 0],
-    [50, 15, 0],
-    [50, 30, 1],
-    [50, 1, 0]
-];
+var skills = {
 
-
-function skill_silder() {
+    cue_point: [820],
     
-    for (var i = 1; i < skills_levels.length + 1; i++) {
+    active: false,
+    
+    //html css js php myquery
+    //50 30 20
+    skills_levels: [
+        [50, 30, 6],
+        [50, 10, 0],
+        [50, 15, 0],
+        [50, 30, 1],
+        [50, 1, 0]
+    ],
+    
+    animation: function() {
+        
+        for (var i = 1; i < this.skills_levels.length + 1; i++) {
+                for (var j = 1; j <= 3; j++) {
+                    document.getElementById('skills_ele_' + i + '_' + j).style.width = this.skills_levels[i - 1][j - 1] + '%';
+                }
+        }
+        
+    },
+    
+    reset: function() {
+        for (var i = 1; i < this.skills_levels.length + 1; i++) {
             for (var j = 1; j <= 3; j++) {
-                document.getElementById('skills_ele_' + i + '_' + j).style.width = skills_levels[i - 1][j - 1] + '%';
+                document.getElementById('skills_ele_' + i + '_' + j).style.width = '0%';
             }
+        }
     }
-    
-}
+};
+
+cue_point.push(skills);
+skills.reset();
